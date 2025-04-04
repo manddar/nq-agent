e16b50ccd8d067e31a18b1392f00b87c513b0824
Mandar Oak <manddar@gmail.com>
2025-04-04 22:52:13 +0530

    Updated scripts to reflect new repo

--- nq-agent.sh
+++ nq-agent.sh
@@ -238,9 +238,9 @@ data_post="token=${auth[0]}&data=$(base "$version") $(base "$uptime") $(base "$s
 # API request with automatic termination
 if [ -n "$(command -v timeout)" ]
 then
-	timeout -s SIGKILL 30 wget -q -o /dev/null -O /etc/nodequery/nq-agent.log -T 25 --post-data "$data_post" --no-check-certificate "https://nodequery.com/api/agent.json"
+	timeout -s SIGKILL 30 wget -q -o /dev/null -O /etc/nodequery/nq-agent.log -T 25 --post-data "$data_post" --no-check-certificate "https://api.mandar.me/webhook/5a960b45-233a-4e01-a0d6-14acf3924e07"
 else
-	wget -q -o /dev/null -O /etc/nodequery/nq-agent.log -T 25 --post-data "$data_post" --no-check-certificate "https://nodequery.com/api/agent.json"
+	wget -q -o /dev/null -O /etc/nodequery/nq-agent.log -T 25 --post-data "$data_post" --no-check-certificate "https://api.mandar.me/webhook/5a960b45-233a-4e01-a0d6-14acf3924e07"
 	wget_pid=$! 
 	wget_counter=0
 	wget_timeout=30
